72486340b8980e44d102ff74d51983ff6b7fa7af
Daniela Gangemi <danielagangemi@live.it>
2024-05-07 17:07:11 +0200

    add: chiamata endpoint di registrazione

--- gestionale_corsi/src/services/RESTservice.jsx
+++ gestionale_corsi/src/services/RESTservice.jsx
@@ -1,4 +1,6 @@
 // ---- USER ENDPOINTS ----
+
+// LOGIN
 export async function login(body) {
 
     const response = await fetch("http://localhost:8080/api/user/login", {
@@ -10,10 +12,33 @@ export async function login(body) {
         body: JSON.stringify(body)
     })
 
-    if(response.status !== 200){
+    if (response.status !== 200) {
         return 0;
     }
 
     return await response.json();
+
+}
+
+// REGISTRATION
+export async function registration(body) {
+
+    console.log(body)
+    console.log(JSON.stringify(body))
+
+    const response = await fetch("http://localhost:8080/api/user/registration", {
+        mode: "cors",
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    })
+
+    if (response.status !== 200) {
+        return 0;
+    }
+
+    return 1;
 
 }
