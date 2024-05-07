0f2cf744d3bb6bc7ddf89657f30d908db10b0e89
Daniela Gangemi <danielagangemi@live.it>
2024-05-07 17:06:40 +0200

    add: aggiunta validazione per nome e cognome

--- gestionale_corsi/src/services/ValidationService.jsx
+++ gestionale_corsi/src/services/ValidationService.jsx
@@ -1,4 +1,4 @@
-export function validateEmail(email){
+export function validateEmail(email) {
 
     const regex = /[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[A-Za-z]{2,24}/
 
@@ -6,10 +6,18 @@ export function validateEmail(email){
 
 }
 
-export function validatePassword(password){
+export function validatePassword(password) {
 
     const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}/;
 
     return password.match(regex)
+
+}
+
+export function validateNameOrSurname(nameOrSurname) {
+
+    const regex = /[a-zA-Z\\s']{1,50}/;
+
+    return nameOrSurname.match(regex);
 
 }
