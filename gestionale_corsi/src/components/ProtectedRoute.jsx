8bbc0900030e4462fe2101102a726d7d672fed02
Daniela Gangemi <danielagangemi@live.it>
2024-05-08 09:00:22 +0200

    feat: creazione del contesto e implementazione del contesto di autenticazione dell'utente

---
+++ gestionale_corsi/src/components/ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+import { AuthContext } from "../context/AuthContext/AuthContext";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function ProtectedRoute({ children }) {
+
+    const { user } = useContext(AuthContext);
+    const navigateTo = useNavigate();
+
+    useEffect(() => {
+
+
+        if (!user.isAuthorized) {
+
+            navigateTo("/")
+
+        }
+
+    })
+
+    return (
+        <>
+            {children}
+        </>
+    );
+
+
+}
